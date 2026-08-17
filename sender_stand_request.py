4677b37b2adc400b180fd58e56fd0265f58e6403
Margarita Gutiérrez <margaritagv07@gmail.com>
2026-08-16 22:07:14 -0600

    Update sender_stand_request.py
    Deleting extra

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -16,32 +16,3 @@ def post_new_client_kit(kit_body, auth_token):
     return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
                         json = kit_body,
                         headers = {"Authorization": "Bearer " + auth_token})
-
-def get_kits_search_by_name(name, auth_token):
-    return requests.get(configuration.URL_SERVICE + configuration.KITS_PATH + "/search",
-                       params={"name": name},
-                       headers={"Authorization": "Bearer " + auth_token})
-
-def get_kits_search_by_products(product_ids, auth_token):
-    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH + "/search",
-                        json={"ids": product_ids},
-                        headers={"Authorization": "Bearer " + auth_token})
-
-def get_client_personal_kits(auth_token):
-    return requests.get(configuration.URL_SERVICE + configuration.KITS_PATH,
-                        headers = {"Authorization": "Bearer " + auth_token})
-
-def put_kit_name(kit_id, body, auth_token):
-    headers = data.headers.copy()
-    headers["Authorization"] = f"Bearer {auth_token}"
-
-    return requests.put(configuration.URL_SERVICE + configuration.KITS_PATH + "/" + str(kit_id),
-                        json=body,
-                        headers=headers)
-
-def delete_kit(kit_id, auth_token):
-    headers = data.headers.copy()
-    headers["Authorization"] = f"Bearer {auth_token}"
-
-    return requests.delete(configuration.URL_SERVICE + configuration.KITS_PATH + "/" + str(kit_id),
-                        headers=headers)
